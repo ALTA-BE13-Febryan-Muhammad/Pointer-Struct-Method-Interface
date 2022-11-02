cc306a8b630f26291ea266613ccf343a84ae5cf8
febryanmz <febryan.zainal@gmail.com>
2022-11-02 07:38:39 +0700

    assignment

--- problem1/problem1.go
+++ problem1/problem1.go
@@ -4,11 +4,13 @@ import "fmt"
 
 func Swap(a, b *int) (int, int) {
 	//your code here
+
 	//pendek
-	var wadahA, wadahB int = *a, *b
-	*a = wadahB
-	*b = wadahA
-	return wadahB, wadahA
+	// var wadahA, wadahB int = *a, *b
+	// *a = wadahB
+	// *b = wadahA
+	// return wadahB, wadahA
+
 	//panjang
 	// wadahA := *a
 	// wadahB := *b
@@ -16,9 +18,11 @@ func Swap(a, b *int) (int, int) {
 	// *b = wadahA
 	// return wadahA, wadahB
 
+	//very short
+	*a, *b = *b, *a
+	return *a, *b
+
 }
-
-
 
 func main() {
 	a := 10
